9733ee71b8fd2f84d30e3722dedcf9312cb416eb
Yufan Sheng <syhily@gmail.com>
2025-11-07 17:51:02 +0800

    feat: rebase for removing the big binary files.

---
+++ src/helpers/content/image/qiniu.ts
@@ -0,0 +1,80 @@
+import type { ExternalImageService, ImageTransform } from 'astro'
+import { baseService } from 'astro/assets'
+import { inferRemoteSize, isESMImportedImage } from 'astro/assets/utils'
+import { getImageMetadata } from '../schema'
+import { isRemoteAllowed } from './assets'
+
+async function getImage(source: string, options: ImageTransform): Promise<{ width: number, height: number, blurhash?: string }> {
+  const { width, height } = options
+  const metadata = getImageMetadata(source)
+  if (metadata) {
+    return { width: width || metadata.width, height: height || metadata.height, blurhash: metadata.blurhash }
+  }
+  if (!width || !height) {
+    const { width, height } = await inferRemoteSize(source)
+    return { width, height }
+  }
+  return { width, height }
+}
+
+const service: ExternalImageService = {
+  ...baseService,
+  async validateOptions(options, imageConfig) {
+    if (!options.width || !options.height || !options.style || !options.style['background-image']) {
+      const imageSource = isESMImportedImage(options.src) ? options.src.src : options.src
+      if (!isRemoteAllowed(imageSource, imageConfig)) {
+        throw new Error(`Image source ${imageSource} is not allowed`)
+      }
+      const { width, height, blurhash } = await getImage(imageSource, options)
+      options.width = options.width || width
+      options.height = options.height || height
+      if (blurhash) {
+        options.style = {
+          'background-image': `url("${blurhash}")`,
+          'background-position': 'center',
+          'background-size': 'cover',
+          'background-repeat': 'no-repeat',
+        }
+      }
+    }
+
+    // Add widths on demand.
+    if (!options.widths || options.widths.length === 0) {
+      options.widths = [
+        500,
+        600,
+        640,
+        750,
+        828,
+        960,
+        1080,
+        1200,
+        1400,
+        1600,
+      ]
+    }
+
+    // Add sizes for images in article.
+    if (!options.fit) {
+      options.fit = 'fill'
+    }
+
+    if (!options.sizes) {
+      options.sizes = `(max-width: 767.98px) 100vw, (max-width: 1200px) 80vw, (max-width: 1600px) 60vw, 70vw`
+    }
+
+    if (typeof baseService.validateOptions === 'function') {
+      return await baseService.validateOptions(options, imageConfig)
+    }
+    throw new Error('baseService.validateOptions is not defined')
+  },
+  async getURL(options, imageConfig) {
+    const imageSource = isESMImportedImage(options.src) ? options.src.src : options.src
+    if (!isRemoteAllowed(imageSource, imageConfig)) {
+      return imageSource
+    }
+    return `${imageSource}?imageView2/1/w/${options.width}/h/${options.height}/format/avif/q/${typeof options.quality === 'number' ? options.quality : 100}/ignore-error/1`
+  },
+}
+
+export default service
---
+++ src/helpers/db/pool.ts
@@ -0,0 +1,14 @@
+import type { NodePgDatabase } from 'drizzle-orm/node-postgres'
+import { DATABASE_URL } from 'astro:env/server'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import * as schema from '@/helpers/db/schema'
+
+const globalForDb = globalThis as unknown as {
+  db: NodePgDatabase<typeof schema> | undefined
+}
+
+export const db = globalForDb.db ?? drizzle(DATABASE_URL, { schema })
+
+if (!globalForDb.db) {
+  globalForDb.db = db
+}
